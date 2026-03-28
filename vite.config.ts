cdacbe014ade8ad9094af5d8291aac85fac0703a
Juan Antonio Navarro <evoratec@gmail.com>
2026-03-28 19:20:03 +0100

    Cambio en docker y tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,23 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
-import { devtools } from "@tanstack/devtools-vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import viteTsConfigPaths from "vite-tsconfig-paths";
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
-const config = defineConfig(({ mode }) => ({
-	server: {
-		port: 3000,
-		host: true,
-		preset: 'node-server',
-	},
+const config = defineConfig({
 	plugins: [
 		devtools(),
-		...(mode === "production"
-			? [
-					cloudflare({
-						configPath: "./wrangler.jsonc",
-						viteEnvironment: { name: "ssr" },
-					}),
-				]
-			: []),
+		nitro(),
+		// this is the plugin that enables path aliases
 		viteTsConfigPaths({
-			projects: ["./tsconfig.json"],
+			projects: ['./tsconfig.json'],
 		}),
 		tailwindcss(),
 		tanstackStart(),
 		viteReact(),
-		tsconfigPaths(),
 	],
-}));
+})
 
-export default config;
+export default config
